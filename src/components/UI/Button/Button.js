4f5b1e0dbe25802b2e2afc22001ae23696a71183
Pauliina Laitinen <pauliina555@hotmail.fi>
2024-12-02 13:00:10 +0200

    tehtävä 26 ja 27

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,9 +1,9 @@
-import styled from 'styled-components';
+import React from 'react';
+import styles from './Button.module.css';
+//import styled from 'styled-components';
 
-//import './Button.css';
-
-const Button = styled.button`
-    
+/* const Button = styled.button`
+      width: 100%;
       font: inherit;
       padding: 0.5rem 1.5rem;
       border: 1px solid #8b005d;
@@ -12,8 +12,12 @@ const Button = styled.button`
       box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
       cursor: pointer;
     
+     @media (min-width: 768px) {
+      width: auto;
+    }
+
     &:focus {
-      outline: none;
+    outline: none;
     }
 
     &:hover,
@@ -22,15 +26,15 @@ const Button = styled.button`
       border-color: #ac0e77;
       box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
     }
-    `;
+    `; */
 
-/*const Button = props => {
+const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
       {props.children}
     </button>
   );
 };
-*/
+
 
 export default Button;
